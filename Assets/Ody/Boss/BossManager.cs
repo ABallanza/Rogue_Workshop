73cbb3163ebebb11789ef4981447fe56ccb9817a
Ody0 <148651602+Ody0@users.noreply.github.com>
2025-02-19 00:15:53 +0100

    asd

--- Assets/Ody/Boss/BossManager.cs
+++ Assets/Ody/Boss/BossManager.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
 
 public class BossManager : MonoBehaviour
 {
@@ -8,6 +10,14 @@ public class BossManager : MonoBehaviour
     public float life = 500f;
     float maxLife;
 
+    [Header("Laser")]
+    public Transform laserPoint;
+
+    public Transform spawnPoint1;
+
+    public LineRenderer lr;
+
+
 
     private void Start()
     {
@@ -15,8 +25,19 @@ public class BossManager : MonoBehaviour
     }
 
 
+    public void FixedUpdate()
+    {
+        Transform player = GameObject.Find("Player").transform;
+        laserPoint.position = Vector3.MoveTowards(laserPoint.position, new Vector3(player.position.x, laserPoint.position.y, player.position.z), 0.1f);
+    }
+
+
+
     public void Update()
     {
+        lr.SetPosition(0, spawnPoint1.position);
+        lr.SetPosition(1, laserPoint.position);
+
         bossLife.value = life / maxLife;
     }
 }
